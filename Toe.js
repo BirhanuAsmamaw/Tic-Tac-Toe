d7a9d2498f4814a40b66ddd5129bd38353046d87
BirhanuAsmamaw <111272781+BirhanuAsmamaw@users.noreply.github.com>
2024-06-11 10:33:56 -0900

    Update Toe.js

--- Toe.js
+++ Toe.js
@@ -46,7 +46,6 @@ function AreaClicked(i){
 
 const Winning_Conditions = [[0,1,2], [3,4,5], [6,7,8], [0,3,6], [1,4,7], [2,5,8], [0,4,8], [2,4,6]]
 
-
 function playerHasWon() {
      for (const condition of Winning_Conditions){
       let [a,b,c] = condition
